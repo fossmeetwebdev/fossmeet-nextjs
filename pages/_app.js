9679e78a042f3d9cddc915cf3c651239509a4e85
Emanuel Christo <emanuelchristochris@gmail.com>
2022-08-14 02:50:22 +0530

    faqs page content

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
 import '../styles/globals.css'
+import '../assets/fonts/jet-brains-mono/jet-brains-mono.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return <Component {...pageProps} />
 }
 
 export default MyApp
