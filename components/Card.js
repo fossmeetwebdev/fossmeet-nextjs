3c53cd06b4b3910eca7779746d39ba7c1bfd9c87
elementalneil <nayek.sankhanil@gmail.com>
2022-08-16 21:36:05 +0530

    Modified Hover Effect on Images

--- components/Card.js
+++ components/Card.js
@@ -1,7 +1,9 @@
+import styles from "../styles/Media.module.css";
+
 const Card = ({img_name}) => {
   let location = "/gallery_images/".concat(img_name);
   return (
-    <div>
+    <div className = {styles.image_container}>
         <img src={location} />
     </div>
   )
