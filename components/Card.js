71ccf30f7e1582ff3092207f7e59ecd8245446cd
Muhammed Jaseem Pallikkal <jaseemseems@gmail.com>
2022-08-26 10:34:59 +0530

    media fixes

--- components/Card.js
+++ components/Card.js
@@ -1,18 +1,22 @@
 import styles from "../styles/Media.module.css";
-import Link from "next/link"
+import Link from "next/link";
 
-const Card = ({img_name}) => {
+const Card = ({ img_name, alt }) => {
   let location = "/gallery_images/".concat(img_name);
   let media_url = "media/?image=".concat(img_name);
   return (
     <div>
-      <div className = {styles.image_container}>
+      <div className={styles.image_container}>
         <Link href={media_url}>
-          <img src={location} className = {styles.image_card}/>
+          <img src={location} alt={alt} className={styles.image_card} />
         </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
+
+Card.defaultProps = {
+  alt: "FOSSMeet picture",
+};
