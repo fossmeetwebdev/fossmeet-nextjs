0bd871b44c953443b88f4118fe245009cc195c97
joelrobin18 <joelrobin1818@gmail.com>
2023-01-03 00:29:49 +0530

    gallery

--- components/LandingPage/Gallery.js
+++ components/LandingPage/Gallery.js
@@ -12,20 +12,26 @@ const Media = () => {
 
       <div className={`{styles.media_body} margin`}>
         <div className={styles.media_gallery}>
-          <Card img_name="1.jpg" />
-          <Card img_name="2.jpg" />
-          <Card img_name="3.jpg" />
-          <Card img_name="4.jpg" />
-          <Card img_name="5.jpg" />
-          <Card img_name="6.jpg" />
-          <Card img_name="7.jpg" />
-          <Card img_name="8.jpg" />
-          <Card img_name="9.jpg" />
-          <Card img_name="10.jpg" />
-          <Card img_name="11.jpg" />
-          <Card img_name="12.jpg" />
-          <Card img_name="13.jpg" />
-          <Card img_name="14.jpg" />
+          <Card img_name="1.webp" />
+          <Card img_name="2.webp" />
+          <Card img_name="3.webp" />
+          <Card img_name="4.webp" />
+          <Card img_name="5.webp" />
+          <Card img_name="6.webp" />
+          <Card img_name="7.webp" />
+          <Card img_name="8.webp" />
+          <Card img_name="9.webp" />
+          <Card img_name="10.webp" />
+          <Card img_name="11.webp" />
+          <Card img_name="12.webp" />
+          <Card img_name="13.webp" />
+          <Card img_name="14.webp" />
+          <Card img_name="15.webp" />
+          <Card img_name="16.webp" />
+          <Card img_name="17.webp" />
+          <Card img_name="18.webp" />
+          <Card img_name="19.webp" />
+          <Card img_name="20.webp" />
         </div>
       </div>
 
