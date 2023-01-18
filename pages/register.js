c444978c273fcd7c3dc4d541f62e3c4b37674df5
Sinadin Shan <sinadinshan66@gmail.com>
2023-01-18 22:13:54 +0530

    Set up register page

--- pages/register.js
+++ pages/register.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Head from 'next/head'
 import Coming from '../components/LandingPage/Coming'
+import styles from '../styles/Register.module.css'
+import Script from 'next/script'
 
 function register() {
   return (
     <div>
-      <Coming/>
+      {/* <Coming/> */}
+      <iframe className={styles.iframe} id="ts-iframe" src="https://www.townscript.com/v2/widget/FOSSMeet23/booking"></iframe> <link rel="stylesheet" href = "https://www.townscript.com/static/Bookingflow/css/ts-iframe.style.css" />
     </div>
   )
 }
