f870ddf87dfa7631c286ec4c0aa6926258f72bf4
Sinadin Shan <sinadinshan66@gmail.com>
2023-01-20 18:14:08 +0530

    Update sponsors

--- components/LandingPage/sponsors.js
+++ components/LandingPage/sponsors.js
@@ -2,7 +2,8 @@ import React from 'react'
 import styles from '../../styles/LandingPage/Sponsor.module.css'
 import Image from 'next/image'
 import img1 from '../../public/images/nilenso1.png'
-import img2 from '/public/images/stickermule-1@2x.png'
+import img2 from '/public/images/FOSS United.svg'
+import Link from 'next/link'
 
 function sponsors() {
   return (
@@ -16,9 +17,12 @@ function sponsors() {
         </h1>
       </div>
       <div className={styles.sponsors_images}>
-        <div className={styles.sponsors_image}>
-          <Image src={img1} width={200} className={styles.img} alt="Nilenso"/>
-        </div>
+        <a href={"http://nilenso.com"} className={styles.sponsors_image} target="_blank" rel='noreferrer'>
+          <Image src={img1} width={200} height={180} className={styles.img} alt="Nilenso"/>
+        </a>
+        <a href={"http://fossunited.org"} className={styles.sponsors_image} target="_blank" rel='noreferrer' >
+          <Image src={img2} width={200} height={180} className={styles.img} alt="FOSS United"/>
+        </a>
       </div>
     </div>
   )
--- pages/register.js
+++ pages/register.js
@@ -5,10 +5,22 @@ import styles from '../styles/Register.module.css'
 import Script from 'next/script'
 
 function register() {
+
+  function preventRedirection(){
+    window.addEventListener('beforeunload', (event) => {
+      // event.preventDefault()
+      alert('redirecting')
+      window.location.replace("http://fossmeet.net")
+    })
+  }
+
   return (
     <div>
       {/* <Coming/> */}
-      <iframe className={styles.iframe} id="ts-iframe" src="https://www.townscript.com/v2/widget/FOSSMeet23/booking"></iframe> <link rel="stylesheet" href = "https://www.townscript.com/static/Bookingflow/css/ts-iframe.style.css" />
+      <iframe onLoad={preventRedirection} className={styles.iframe} id="ts-iframe" src="https://www.townscript.com/v2/widget/FOSSMeet23/booking"></iframe> <link rel="stylesheet" href = "https://www.townscript.com/static/Bookingflow/css/ts-iframe.style.css" />
+      {/* <script id='script'>
+        {preventRedirection()}
+      </script> */}
     </div>
   )
 }
