b8ecb39bf3df9c6b14d3b8d5f41c64110e6f6f7f
Sinadin Shan <sinadinshan66@gmail.com>
2023-01-23 18:26:22 +0530

    Update sponsors

--- components/LandingPage/sponsors.js
+++ components/LandingPage/sponsors.js
@@ -3,6 +3,8 @@ import styles from '../../styles/LandingPage/Sponsor.module.css'
 import Image from 'next/image'
 import img1 from '../../public/images/nilenso1.png'
 import img2 from '/public/images/FOSS United.svg'
+import img3 from '/public/images/ICFOSS.png'
+import img4 from '/public/images/FSF.png'
 import Link from 'next/link'
 
 function sponsors() {
@@ -18,10 +20,16 @@ function sponsors() {
       </div>
       <div className={styles.sponsors_images}>
         <a href={"http://nilenso.com"} className={styles.sponsors_image} target="_blank" rel='noreferrer'>
-          <Image src={img1} width={200} height={180} className={styles.img} alt="Nilenso"/>
+          <Image src={img1} width={240} height={120} className={styles.img} alt="Nilenso"/>
         </a>
         <a href={"http://fossunited.org"} className={styles.sponsors_image} target="_blank" rel='noreferrer' >
-          <Image src={img2} width={200} height={180} className={styles.img} alt="FOSS United"/>
+          <Image src={img2} width={150} height={120} className={styles.img} alt="FOSS United"/>
+        </a>
+        <a href={"http://icfoss.in"} className={styles.sponsors_image} target="_blank" rel='noreferrer' >
+          <Image src={img3} width={200} height={180} className={styles.img} alt="ICFOSS"/>
+        </a>
+        <a href={"https://fsf.org"} className={styles.sponsors_image} target="_blank" rel='noreferrer' >
+          <Image src={img4} width={200} height={180} className={styles.img} alt="FSF"/>
         </a>
       </div>
     </div>
